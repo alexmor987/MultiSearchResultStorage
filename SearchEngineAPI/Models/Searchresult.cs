a6f0dedeb937c3fe50c48e9ad5bbaa3da2ff9b06
Alex Mordasov <alexmor987@gmail.com>
2024-03-20 00:37:16 +0200

    Initial commit

---
+++ SearchEngineAPI/Models/Searchresult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SearchEngineAPI.Models;
+
+public partial class SearchResult
+{
+    public int Id { get; set; }
+
+    public string SearchEngine { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public DateTime Entereddate { get; set; }
+}
